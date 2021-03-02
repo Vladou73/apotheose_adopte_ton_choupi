726fa3ff8978ef72c35accba2b760f596bac5ea0
Elisa Greder <elisagreder.dev@gmail.com>
2021-03-02 13:41:12 +0100

    setting up of the adoption route

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -4,11 +4,20 @@ import { Route, Switch } from 'react-router-dom';
 
 // == Import
 import './styles.scss';
+import Adoption from '../Adoption';
 
 // == Composant
 const App = () => (
   <div className="app">
     <h1>Projet SPA</h1>
+    <Switch >
+    <Route path="/" exact>
+      // Composant Home
+    </Route>
+      <Route path="/animaux" exact>
+        <Adoption />
+      </Route>
+    </Switch>
   </div>
 );
 
