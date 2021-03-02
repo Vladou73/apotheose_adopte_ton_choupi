d3837c9e64e469474b83995f0505cec5c90fc517
Romain <evanxprod@gmail.com>
2021-03-02 16:35:13 +0100

    Footer done

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -5,19 +5,21 @@ import { Route, Switch } from 'react-router-dom';
 // == Import
 import './styles.scss';
 import Adoption from '../Adoption';
+import Footer from '../Footer';
 
 // == Composant
 const App = () => (
   <div className="app">
     <h1>Projet SPA</h1>
-    <Switch >
-    <Route path="/" exact>
-      // Composant Home
-    </Route>
+    <Switch>
+      <Route path="/" exact>
+        {/* Composant Home */}
+      </Route>
       <Route path="/animaux" exact>
         <Adoption />
       </Route>
     </Switch>
+    <Footer />
   </div>
 );
 
