6a11517a5a9711d4804896d63ae9e889004c01b9
Romain <evanxprod@gmail.com>
2021-03-02 11:53:39 +0100

    react template base

---
+++ front/src/components/App/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// == Import
+import './styles.scss';
+import './reset.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>Projet SPA</h1>
+  </div>
+);
+
+// == Export
+export default App;
