80174d301213790f172bac8621bde9edabac4571
Romain <evanxprod@gmail.com>
2021-03-02 20:39:17 +0100

    Header done (ready for testing)

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -2,54 +2,55 @@ import React from 'react';
 import './style.scss';
 import { NavLink } from 'react-router-dom';
 import Logo from './logo.png';
+import AdopterSubmenu from './AdopterSubmenu';
+import AgirSubmenu from './AgirSubmenu';
 
 const Header = () => (
   <div className="header">
-    <img src={Logo} alt="Logo adopte ton choupi" />
-    <h1 className="title">Adopte ton choupi</h1>
-    <nav className="menu">
-      <NavLink
-        className="menu-link"
-        to="/"
-        activeClassName="menu-link--active"
-        exact
-      >
-        Accueil
-      </NavLink>
-      <NavLink
-        className="menu-link"
-        to="/animaux"
-        activeClassName="menu-link--active"
-        exact
-      >
-        Adopter
-      </NavLink>
-      <NavLink
-        className="menu-link"
-        to="/articles"
-        activeClassName="menu-link--active"
-        exact
-      >
-        Blog
-      </NavLink>
-      <NavLink
-        className="menu-link"
-        to="/agir"
-        activeClassName="menu-link--active"
-        exact
-      >
-        Agir
-      </NavLink>
-      <NavLink
-        className="menu-link"
-        to="/contact"
-        activeClassName="menu-link--active"
-        exact
-      >
-        Contact
-      </NavLink>
-      <a href="#"><span className="donate">Faire un don</span></a>
+    <a href="/"><img src={Logo} alt="Logo adopte ton choupi" />
+      <h1 className="title">Adopte ton choupi</h1>
+    </a>
+    <nav className="nav">
+      <ul className="nav__menu">
+        <li className="nav__menu-item">
+          <NavLink
+            to="/"
+            exact
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li
+          className="nav__menu-item"
+        >
+          <a>Adopter</a>
+          <AdopterSubmenu />
+        </li>
+        <li
+          className="nav__menu-item"
+        >
+          <NavLink
+            to="/articles"
+            exact
+          >
+            Blog
+          </NavLink>
+        </li>
+        <li className="nav__menu-item">
+          <a>Agir</a>
+          <AgirSubmenu />
+        </li>
+        <li className="nav__menu-item">
+          <NavLink
+            to="/contact"
+            exact
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
     </nav>
+    <a href="/don"><span className="donate">Faire un don</span></a>
   </div>
 );
 
