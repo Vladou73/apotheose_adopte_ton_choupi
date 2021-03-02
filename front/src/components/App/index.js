859c25c9750c3238f9c3798ed5a34e1e40b3bc2c
Maneldj <manel.djedir@yahoo.fr>
2021-03-02 13:49:03 +0100

    Initial commit

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -10,10 +10,10 @@ import Adoption from '../Adoption';
 const App = () => (
   <div className="app">
     <h1>Projet SPA</h1>
-    <Switch >
-    <Route path="/" exact>
-      // Composant Home
-    </Route>
+    <Switch>
+      <Route path="/" exact>
+        */ Composant Home */
+      </Route>
       <Route path="/animaux" exact>
         <Adoption />
       </Route>
