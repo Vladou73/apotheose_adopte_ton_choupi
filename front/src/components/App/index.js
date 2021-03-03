d72d7ffccd6029fa9a89e51ff894611cfe268253
Maneldj <manel.djedir@yahoo.fr>
2021-03-03 10:23:30 +0100

    Home page without css

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -3,16 +3,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // == Import
-import './styles.scss';
 import Adoption from '../Adoption';
+import Home from '../Home';
+
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <h1>Projet SPA</h1>
+    <h1 className="maintitle">Projet SPA</h1>
     <Switch>
       <Route path="/" exact>
-        */ Composant Home */
+        <Home />
       </Route>
       <Route path="/animaux" exact>
         <Adoption />
