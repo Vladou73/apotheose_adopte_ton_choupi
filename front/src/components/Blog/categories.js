8c111a3100c553fa18b85fe72d3c011d878504a0
Romain <evanxprod@gmail.com>
2021-03-03 14:28:58 +0100

    components articles and categories added

---
+++ front/src/components/Blog/categories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+const Categories = () => (
+  <>
+    <a href="#"><span className="blog__categories__category">Catégorie1</span></a>
+    <a href="#"><span className="blog__categories__category">Catégorie2</span></a>
+    <a href="#"><span className="blog__categories__category">Catégorie3</span></a>
+    <a href="#"><span className="blog__categories__category">Catégorie4</span></a>
+    <a href="#"><span className="blog__categories__category">Catégorie5</span></a>
+  </>
+);
+
+export default Categories;
