07a088f4e9fe4687da2bbc00051a7e182a9e52c3
Romain <evanxprod@gmail.com>
2021-03-04 15:37:49 +0100

    prepared components for fake categories

--- front/src/components/Blog/index.js
+++ front/src/components/Blog/index.js
@@ -4,18 +4,21 @@ import Categories from './categories';
 import Article from './article';
 import './style.scss';
 
-const Blog = ({ datas }) => (
-  <div className="blog">
-    <h2 className="blog__title">Nos articles</h2>
-    <div className="blog__categories">
-      <p className="blog__categories__title">Les catégories</p>
-      <Categories />
+const Blog = ({ datas }) => {
+  console.log(datas);
+  return (
+    <div className="blog">
+      <h2 className="blog__title">Nos articles</h2>
+      <div className="blog__categories">
+        <p className="blog__categories__title">Les catégories</p>
+        <Categories />
+      </div>
+      <div className="blog__articles">
+        {datas.map((data) => <Article key={data.id} {...data} />)}
+      </div>
     </div>
-    <div className="blog__articles">
-      {datas.map((data) => <Article key={data.id} {...data} />)}
-    </div>
-  </div>
-);
+  );
+};
 
 Blog.propTypes = {
   datas: PropTypes.arrayOf(
