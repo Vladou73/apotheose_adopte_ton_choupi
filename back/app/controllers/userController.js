b29ade6706839891f76e03af4c502107a6992670
Vladou73 <vladimir.nafissi@essec.edu>
2021-03-05 18:50:43 +0100

    added routes admin and admin/manageAnimals, userController and userServices, and JWT signin and authentif. Not linked to db yet

---
+++ back/app/controllers/userController.js
@@ -0,0 +1,23 @@
+const { response } = require('express');
+const userService = require('../services/userService');
+
+userController = {
+    signIn: (request, response, next) => {
+        console.log('entered userController.signIn');
+        console.log('request.body ',request.body)
+        userService.signIn(request.body)
+            .then(user => response.json(user))
+            .catch(next);
+    },
+    authenticate : (request, response, next) => {
+        console.log('entered userController.authenticate');
+        console.log('request.headers ',request.headers);
+        console.log('request.body ',request.body);
+        userService.authenticate(request.headers)
+            .then(decoded => response.json(decoded))
+            .catch(next);
+        // next();
+    }
+}
+
+module.exports = userController;
---
+++ back/app/services/userService.js
@@ -0,0 +1,52 @@
+const config = require('../authentification/config.json');
+const jwt = require('jsonwebtoken');
+
+// users hardcoded for simplicity, store in a db for production applications
+const users = [
+    { id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }
+];
+
+userService = {
+    signIn : async function ({ username, password }) {
+        const user = users.find(u => u.username === username && u.password === password);
+    
+        if (!user) throw 'Username or password is incorrect';
+    
+        // create a jwt token that is valid for 1 day
+        const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '1d' });
+        console.log(token);
+        
+        return {
+            ...omitPassword(user),
+            token
+        };
+    },
+    authenticate : async function (headers) {
+        const authHeader = headers.authorization;
+
+        if (!authHeader) throw 'Username or password is incorrect';
+
+        // get token from authHeader
+        const token = authHeader.split(' ')[1];
+        console.log('token', token);
+
+        //check token is valid
+        try {
+            const decoded = jwt.verify(token, config.secret)
+            console.log(decoded);
+            return decoded;
+        } catch(err) {
+            throw err
+        }
+    }
+
+}
+
+// helper functions
+
+function omitPassword(user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
+
+module.exports = userService;
