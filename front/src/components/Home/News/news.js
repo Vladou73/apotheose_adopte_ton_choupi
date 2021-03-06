119be53c27ce83f527520d528ef0a0c1830d586c
Elisa Greder <elisagreder.dev@gmail.com>
2021-03-06 14:13:41 +0100

    Home responvise / desktop design needs improvement but mobile ok

--- front/src/components/Home/News/news.js
+++ front/src/components/Home/News/news.js
@@ -1,18 +1,38 @@
-/* eslint-disable max-len */
 // == Import npm
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // == Import
-import profileLogo from './profilePicture.png';
 import './styles.scss';
 
 // == Composant
 const News = () => (
-  <div className="box">
-    <img src={profileLogo} className="profileLogo" alt="profile logo" />
-    <p className="individualNews__text"> Eodem tempore Serenianus ex duce, cuius ignavia populatam in Phoenice Celsen ante rettulimus, pulsatae maiestatis imperii reus iure postulatus ac lege, incertum qua potuit suffragatione absolvi, aperte convictus familiarem suum cum pileo, quo caput operiebat, incantato vetitis artibus ad templum misisse fatidicum, quaeritatum expresse an ei firmum portenderetur imperium, ut cupiebat, et cunctum.
-    </p>
-  </div>
+  <Link
+    to="/article/:id"
+    className="home-news__link"
+  >
+    <div className="home-news__box">
+      <img
+        className="home-news__picture"
+        src="xxx"
+        alt="article"
+      />
+      <div className="home-news__contain">
+        <p className="home-news__title">Titre</p>
+        <span className="home-news__category">Catégorie</span>
+        <p className="home-news__text">
+          Eodem tempore Serenianus ex duce, cuius ignavia
+          populatam in Phoenice Celsen ante rettulimus,
+          pulsatae maiestatis imperii reus iure postulatus ac lege,
+          incertum qua potuit suffragatione absolvi,
+          aperte convictus familiarem suum
+          cum pileo, quo caput operiebat, incantato vetitis artibus ad templum misisse fatidicum,
+          quaeritatum expresse an ei firmum portenderetur imperium, ut cupiebat, et cunctum.
+        </p>
+        <p className="home-news__more">En savoir d'avantage..</p>
+      </div>
+    </div>
+  </Link>
 );
 
 // == Export
