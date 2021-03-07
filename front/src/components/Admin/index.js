b10a745ecc08d985254b04fad115b415caae1b74
Romain <evanxprod@gmail.com>
2021-03-07 21:24:38 +0100

    css for admin page connection

--- front/src/components/Admin/index.js
+++ front/src/components/Admin/index.js
@@ -10,12 +10,12 @@ const Admin = () => {
 
   return (
     <div className="admin">
-      <h1 className="admin__title">Espace administration</h1>
+      <h1 className="admin__title">Espace Administration</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username" className="admin__username">Nom d'utilisateur :</label>
-        <input id="username" name="username" />
+        <input id="username" name="username" className="admin__username__input" />
         <label htmlFor="password" className="admin__password">Mot de passe :</label>
-        <input id="password" name="password" />
+        <input id="password" name="password" className="admin__password__input" />
         <button type="submit" className="admin__submit">Connexion</button>
       </form>
     </div>
