f5af340e32af8379c928a51345b126851ec79754
Romain <evanxprod@gmail.com>
2021-03-10 15:15:19 +0100

    list articles admin done

--- front/src/components/ManageArticles/index.js
+++ front/src/components/ManageArticles/index.js
@@ -4,10 +4,33 @@ import { Link } from 'react-router-dom';
 // == Import
 import './styles.scss';
 
-const ManageArticles = () => (
-  <div>
-    <Link to="/admin/gestion-articles/1"> articles 1 </Link>
-    <p>Gestion articles</p>
+const ManageArticles = ({ articles }) => (
+  <div className="manageArticles">
+    <Link to="/admin/gestion-articles/1" className="manageArticles__link"> articles 1 </Link>
+    <Link to="#" className="manageArticles__link__add"> Ajouter </Link>
+    <Link to="#" className="manageArticles__link__delete"> Supprimer </Link>
+    <table className="manageArticles__table">
+      <tbody>
+        <tr>
+          <td />
+          <td>Auteur</td>
+          <td>Titre</td>
+          <td>Date</td>
+          <td>Tag</td>
+        </tr>
+        {articles.map((article) => (
+          <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>{article.author_firstname} {article.author_lastname}</td>
+            <td>{article.title}</td>
+            <td>{article.created_at}</td>
+            <td>{article.category_name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </div>
 );
 
