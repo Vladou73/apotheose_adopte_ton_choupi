9b30033446af9bee31e87f94c121133b61738c0b
Elisa Greder <elisagreder.dev@gmail.com>
2021-03-13 20:37:43 +0100

    cards adoption border radius

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -20,11 +20,9 @@ const Home = () => (
       <h2 className="home__subtitle"> Société protectrice des animaux</h2>
       <div className="home__paragraph">
         <p>
-          Soleo saepe ante oculos ponere, idque libenter crebris usurpare sermonibus, omnis nostrorum imperatorum, omnis exterarum gentium potentissimorumque populorum, omnis clarissimorum regum res gestas, cum tuis nec contentionum magnitudine nec numero proeliorum nec varietate regionum nec celeritate conficiendi nec dissimilitudine bellorum posse conferri; nec vero disiunctissimas terras citius passibus cuiusquam potuisse peragrari, quam tuis non dicam cursibus, sed victoriis lustratae sunt.
-          Ego vero sic intellego, Patres conscripti, nos hoc tempore in provinciis decernendis perpetuae pacis habere oportere rationem.
+          Le premier combat de la SPA créée en 1845 par Etienne Pariset portait sur la protection des chevaux que les cochers parisiens maltraitaient. Au fil du temps, notre action s'est developpée et notre intérêt s'est élargi aux autres éspèces animales, en particulier nos plus proches compagnons, les chiens et les chats.
         </p>
-        <p>Nam quis hoc non sentit omnia alia esse nobis vacua ab omni periculo atque etiam suspicione belli?
-          Mensarum enim voragines et varias voluptatum inlecebras, ne longius progrediar, praetermitto illuc transiturus quod quidam per ampla spatia urbis subversasque silices sine periculi metu properantes equos velut publicos signatis quod dicitur calceis agitant, familiarium agmina tamquam praedatorios globos post terga trahentes ne Sannione quidem, ut ait comicus, domi relicto. quos imitatae matronae complures opertis capitibus et basternis per latera civitatis cuncta discurrunt.
+        <p>L’association a pour but d’améliorer, par tous les moyens qui sont en son pouvoir, le sort de tous les animaux, de lutter contre leur trafic, de veiller à ce que soient respectées les dispositions législatives et réglementaires qui les protègent et de leur accorder assistance, et de participer en ce sens à la sensibilisation de l’opinion publique.
         </p>
       </div>
       <p className="home__subtitle">Suivez nos actualités</p>
