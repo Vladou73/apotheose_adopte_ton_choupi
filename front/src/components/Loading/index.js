d29a230a1c77bf7d25f29fdba75cd8af74ad8b42
Elisa Greder <elisagreder.dev@gmail.com>
2021-03-13 15:39:09 +0100

    loading dog

--- front/src/components/Loading/index.js
+++ front/src/components/Loading/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import './style.scss';
 
-export default () => <div className="spinner" />;
+export default () => (
+  <div id="dots">
+    <div id="dot1" className="dot" />
+    <div id="dot2" className="dot" />
+    <div id="dot3" className="dot" />
+    <div id="dot4" className="dot" />
+    <div id="dot5" className="dot5" />
+  </div>
+);
