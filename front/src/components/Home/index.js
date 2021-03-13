76a8a591beb7c1ce2a2288ec8d94976cb27746ca
Elisa Greder <elisagreder.dev@gmail.com>
2021-03-13 17:52:49 +0100

    responsive articles home

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -27,9 +27,9 @@ const Home = () => (
           Mensarum enim voragines et varias voluptatum inlecebras, ne longius progrediar, praetermitto illuc transiturus quod quidam per ampla spatia urbis subversasque silices sine periculi metu properantes equos velut publicos signatis quod dicitur calceis agitant, familiarium agmina tamquam praedatorios globos post terga trahentes ne Sannione quidem, ut ait comicus, domi relicto. quos imitatae matronae complures opertis capitibus et basternis per latera civitatis cuncta discurrunt.
         </p>
       </div>
+      <p className="home__subtitle">Suivez nos actualités</p>
     </div>
     <section className="home__section">
-      <p className="home__subtitle">Suivez nos actualités</p>
       <div className="home__news">
         <News />
         <News />
