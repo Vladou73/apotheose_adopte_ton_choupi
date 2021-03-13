bf222e68afd597909a07c472ee692d7b0b6e16e1
Elisa Greder <elisagreder.dev@gmail.com>
2021-03-13 21:08:04 +0100

    line-heigh

--- front/src/components/Participate/index.js
+++ front/src/components/Participate/index.js
@@ -13,23 +13,33 @@ import './styles.scss';
 const Participate = () => (
   <div className="participate">
     <main className="mainParticipate">
-      <h2 className="participateTitle"> Participer occasionnellement </h2>
+      <h2 className="participateTitle"> Devenir bénévole au sein de notre association</h2>
       <p className="introText">
-        Haec dum oriens diu perferret, caeli reserato tepore Constantius consulatu suo septies et Caesaris ter egressus Arelate Valentiam petit, in Gundomadum et Vadomarium fratres Alamannorum reges arma moturus, quorum crebris excursibus vastabantur confines limitibus terrae Gallorum.
+        S’engager auprès de refuge pour venir en aide aux animaux est une formidable expérience et un enrichissement personnel. C’est aussi rejoindre la plus ancienne association de protection animale dont l’expérience et le savoir-faire nous permettent de secourir, soigner et faire adopter des milliers d’animaux chaque année.
       </p>
       <img src={participatePicture} className="participatePicture" alt=" join us" />
       <div className="sponsorElements">
-        <p className="subTitleSponsor"> Parrainer un animal </p>
+        <p className="subTitleSponsor"> Bien-etre des animaux </p>
         <p className="sponsorText">
           Per hoc minui studium suum existimans Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perduceret: quo percitus ille exitio urgente abrupto ferro eundem adoritur Paulum. et quia languente dextera, letaliter ferire non potuit, iam districtum mucronem in proprium latus inpegit. hocque deformi genere mortis excessit e vita iustissimus rector ausus miserabiles casus levare multorum.
-          Nunc vero inanes flatus quorundam vile esse quicquid extra urbis pomerium nascitur aestimant praeter orbos et caelibes, nec credi potest qua obsequiorum diversitate coluntur homines sine liberis Romae.
         </p>
       </div>
       <div className="walkingElements">
-        <p className="subTitleWalking"> Promener les animaux </p>
+        <p className="subTitleWalking"> Entretien des animaux </p>
         <p className="walkingText">
-          Ut enim quisque sibi plurimum confidit et ut quisque maxime virtute et sapientia sic munitus est, ut nullo egeat suaque omnia in se ipso posita iudicet, ita in amicitiis expetendis colendisque maxime excellit. Quid enim? Africanus indigens mei? Minime hercule! ac ne ego quidem illius; sed ego admiratione quadam virtutis eius, ille vicissim opinione fortasse non nulla, quam de meis moribus habebat, me dilexit; auxit benevolentiam consuetudo. Sed quamquam utilitates multae et magnae consecutae sunt, non sunt tamen ab earum spe causae diligendi profectae.
-          Nec piget dicere avide magis hanc insulam populum Romanum invasisse quam iuste. Ptolomaeo enim rege foederato nobis et socio ob aerarii nostri angustias iusso sine ulla culpa proscribi ideoque hausto veneno voluntaria morte deleto et tributaria facta est et velut hostiles eius exuviae classi inpositae in urbem advectae sunt per Catonem, nunc repetetur ordo gestorum.
+          Ut enim quisque sibi plurimum confidit et ut quisque maxime virtute et sapientia sic munitus est, ut nullo egeat suaque omnia in se ipso posita iudicet, ita in amicitiis expetendis colendisque maxime excellit. Quid enim? Africanus indigens mei? Minime hercule! ac ne ego quidem illius; sed ego admiratione quadam virtutis eius, ille vicissim opinione fortasse non nulla, quam de meis moribus habebat, me dilexit; auxit benevolentiam consuetudo.
+        </p>
+      </div>
+      <div className="sponsorElements">
+        <p className="subTitleSponsor"> Animation et Sensibilisation à la cause animale </p>
+        <p className="sponsorText">
+          Per hoc minui studium suum existimans Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perduceret: quo percitus ille exitio urgente abrupto ferro eundem adoritur Paulum. et quia languente dextera, letaliter ferire non potuit, iam districtum mucronem in proprium latus inpegit. hocque deformi genere mortis excessit e vita iustissimus rector ausus miserabiles casus levare multorum.
+        </p>
+      </div>
+      <div className="sponsorElements">
+        <p className="subTitleSponsor"> Administration bureautique </p>
+        <p className="sponsorText">
+          Per hoc minui studium suum existimans Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perduceret: quo percitus ille exitio urgente abrupto ferro eundem adoritur Paulum.
         </p>
       </div>
       <Link to="/contact" className="contactButton">Nous contacter</Link>
