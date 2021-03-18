d17ca10e0302754124dcb57b80c312feeb54d7b1
Maneldj <manel.djedir@yahoo.fr>
2021-03-18 09:12:57 +0100

    Without the link

--- front/src/components/Creator/index.js
+++ front/src/components/Creator/index.js
@@ -9,38 +9,30 @@ const Creator = () => (
   <section className="section-creator">
     <h2 className="section-creator__title"> </h2>
     <div className="cards-creator"> 
-      <a href="/creator/1">
         <div class="card-creator">
           <h3 className="card-creator__name"> Elisa GREDER</h3>
           <img class="card-creator__picture" src="https://firebasestorage.googleapis.com/v0/b/adopte-ton-choupi.appspot.com/o/images%2Fpexels-andreas-schmolmueller-3376610.jpg?alt=media&amp;token=531701c3-e297-4659-886f-efa7a8bf184c" alt="Elisa Greder"></img>
           <p className="card-creator__span"> Rôle:</p>
-          <p className="card-creator__span">Product Owner / Développeur Front </p>
+          <p className="card-creator__span">Product Owner / Dev Front </p>
         </div>
-      </a>
-      <a href="/creator/2">
         <div class="card-creator">
           <h3 className="card-creator__name"> Manel DJEDIR</h3>
           <img class="card-creator__picture" src="https://firebasestorage.googleapis.com/v0/b/adopte-ton-choupi.appspot.com/o/images%2Fpexels-andreas-schmolmueller-3376610.jpg?alt=media&amp;token=531701c3-e297-4659-886f-efa7a8bf184c" alt="Manel Djedir"></img>
           <p className="card-creator__span"> Rôle:</p>
-          <p className="card-creator__span">Scrum Master / Développeur Front </p>
+          <p className="card-creator__span">Scrum Master / Dev Front </p>
         </div>
-      </a>
-      <a href="/creator/3">
         <div class="card-creator">
           <h3 className="card-creator__name"> Romain BEGEY</h3>
           <img class="card-creator__picture" src="https://firebasestorage.googleapis.com/v0/b/adopte-ton-choupi.appspot.com/o/images%2Fpexels-andreas-schmolmueller-3376610.jpg?alt=media&amp;token=531701c3-e297-4659-886f-efa7a8bf184c" alt="Romain Begey"></img>
           <p className="card-creator__span"> Rôle:</p>
-          <p className="card-creator__span">Leader Développeur Front </p>
+          <p className="card-creator__span">Lead Dev Front </p>
         </div>
-      </a>
-      <a href="/creator/4">
         <div class="card-creator">
           <h3 className="card-creator__name"> Vladimir NAFISSI</h3>
           <img class="card-creator__picture" src="https://firebasestorage.googleapis.com/v0/b/adopte-ton-choupi.appspot.com/o/images%2Fpexels-andreas-schmolmueller-3376610.jpg?alt=media&amp;token=531701c3-e297-4659-886f-efa7a8bf184c" alt="Vladimir Nafissi"></img>
           <p className="card-creator__span"> Rôle:</p>
-          <p className="card-creator__span">Leader Développeur Back </p>
+          <p className="card-creator__span">Lead Dev Back </p>
         </div>
-      </a>
     </div>
   </section>
   </>
