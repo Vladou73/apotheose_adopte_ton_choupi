82eecd83626845682dbb8bed64c638d0cae3d836
Maneldj <manel.djedir@yahoo.fr>
2021-03-18 13:58:07 +0100

    Error in inspector for animals ok

--- front/src/components/Join/index.js
+++ front/src/components/Join/index.js
@@ -30,36 +30,36 @@ const Join = () => (
       <div className="sponsorElements">
         <h2 className="subTitleSponsor"> Entretien et bien-être des animaux </h2>
         <p className="sponsorText">
-          <h3 className="subTitleSponsor-subtitle">En adoptant un animal je m'engage:</h3>
-          À le respecter, à lui apporter toute l’affection et l’attention dont il a besoin et à ne jamais l’abandonner,tous les soins nécessaires pour le maintenir en bonne santé a fournir des conditions de vie et d’hébergement assurant son bien-être et a respecter les obligations que m’impose la loi si mon animal est un chien de deuxième catégorie.<br />
+        <h3 className="subTitleSponsor-subtitle">En adoptant un animal je m'engage:</h3>
+            À le respecter, à lui apporter toute l’affection et l’attention dont il a besoin et à ne jamais l’abandonner,tous les soins nécessaires pour le maintenir en bonne santé a fournir des conditions de vie et d’hébergement assurant son bien-être et a respecter les obligations que m’impose la loi si mon animal est un chien de deuxième catégorie.<br />
         </p>
         <p className="sponsorText">
-          <h3 className="subTitleSponsor-subtitle">A son arrivée, j’accueille mon animal dans des conditions permettant la réussite de son adoption :</h3>
-          <ul className="join-ul">
-            <li className="join-li">Je suis vigilant à son comportement (stress de quitter le refuge et de se retrouver dans un nouvel environnement) afin d’éviter les risques de morsures/griffures et de fugues.</li>
-            <li className="join-li">J’apprends aux enfants à respecter l’animal en tant que membre de la famille.</li>
-            <li className="join-li">Je suis compréhensif et patient avec un animal adopté qui a pu subir des traumatismes par le passé (abandon, violence…).</li>
-          </ul>
+        <h3 className="subTitleSponsor-subtitle">A son arrivée, j’accueille mon animal dans des conditions permettant la réussite de son adoption :</h3>
+        <ul className="join-ul">
+          <li className="join-li">Je suis vigilant à son comportement (stress de quitter le refuge et de se retrouver dans un nouvel environnement) afin d’éviter les risques de morsures/griffures et de fugues.</li>
+          <li className="join-li">J’apprends aux enfants à respecter l’animal en tant que membre de la famille.</li>
+          <li className="join-li">Je suis compréhensif et patient avec un animal adopté qui a pu subir des traumatismes par le passé (abandon, violence…).</li>
+        </ul>
         </p>
         <p className="sponsorText">
-          <h3 className="subTitleSponsor-subtitle">A son arrivée, j’accueille mon animal dans des conditions permettant la réussite de son adoption :</h3>
-          <ul>
-            <li className="join-li">J’éduque mon animal en lui donnant une place dans la maison, mais pas toute la place !</li>
-            <li className="join-li">Je l’habitue à mon environnement et aux différents bruits.</li>
-            <li className="join-li">Je le stimule par le jeu et je le balade le plus possible s’il s’agit d’un chien.</li>
-            <li className="join-li">Je le récompense de manière positive quand il se comporte bien.</li>
-            <li className="join-li">Je lui apprends la propreté.</li>
-            <li className="join-li">Je suis ferme et je sais dire NON, par le ton et sans violence.</li>
-          </ul>
+        <h3 className="subTitleSponsor-subtitle">A son arrivée, j’accueille mon animal dans des conditions permettant la réussite de son adoption :</h3>
+        <ul>
+          <li className="join-li">J’éduque mon animal en lui donnant une place dans la maison, mais pas toute la place !</li>
+          <li className="join-li">Je l’habitue à mon environnement et aux différents bruits.</li>
+          <li className="join-li">Je le stimule par le jeu et je le balade le plus possible s’il s’agit d’un chien.</li>
+          <li className="join-li">Je le récompense de manière positive quand il se comporte bien.</li>
+          <li className="join-li">Je lui apprends la propreté.</li>
+          <li className="join-li">Je suis ferme et je sais dire NON, par le ton et sans violence.</li>
+        </ul>
         </p>
         <p className="sponsorText">
-          <h3 className="subTitleSponsor-subtitle">Les soins :</h3>
-          <ul>
-            <li className="join-li">Je le fais vacciner et suivre régulièrement par le vétérinaire.</li>
-            <li className="join-li">Je le fais stériliser et lui applique un traitement antiparasitaire.</li>
-            <li className="join-li">J’entretiens son pelage et je vérifie ses pattes et ses oreilles.</li>
-            <li className="join-li">Je le nourris bien et je lui laisse de l’eau fraîche et propre en permanence.</li>
-          </ul>
+        <h3 className="subTitleSponsor-subtitle">Les soins :</h3>
+        <ul>
+          <li className="join-li">Je le fais vacciner et suivre régulièrement par le vétérinaire.</li>
+          <li className="join-li">Je le fais stériliser et lui applique un traitement antiparasitaire.</li>
+          <li className="join-li">J’entretiens son pelage et je vérifie ses pattes et ses oreilles.</li>
+          <li className="join-li">Je le nourris bien et je lui laisse de l’eau fraîche et propre en permanence.</li>
+        </ul>
         </p>
       </div>
       <div className="sponsorElements">
